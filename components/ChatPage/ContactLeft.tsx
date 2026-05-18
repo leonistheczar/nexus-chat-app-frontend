b49d330212913466606e0f455f32545e2a354849
Muhammad Ali <alisdigitalwork@gmail.com>
2026-05-18 13:57:22 +0500

    Developed the ChatLeft component displaying contacts (currently using mock-json data)

--- components/ChatPage/ContactLeft.tsx
+++ components/ChatPage/ContactLeft.tsx
@@ -3,6 +3,7 @@
 import { CirclePlus, EllipsisVertical } from "lucide-react";
 import { useEffect, useState } from "react";
 import ThemeToggler from "../baseComponents/ThemeToggler";
+import Image from "next/image";
 
 export default function ContactLeft() {
   const [contacts, setContacts] = useState<any[]>([]);
@@ -21,11 +22,11 @@ export default function ContactLeft() {
   }, []);
   const filteredContacts = contacts.slice(0, 10);
   return (
-    <div className="container py-4 px-6 flex flex-col gap-y-2 border-r border-primary-200 h-screen">
-      <div id="contact-base" className="flex justify-between items-center">
-        <h1 className="text-2xl">Nexus</h1>
-        <div className="flex items-center gap-x-2">
-            <ThemeToggler />
+    <div className="container bg-primary-100 flex flex-col border-r border-primary-200 h-screen">
+      <div id="contact-base" className="flex justify-between items-center px-4 py-2">
+        <h1 className="text-xl">Nexus</h1>
+        <div className="flex items-center gap-x-2 scale-90">
+          <ThemeToggler />
           <button className="hover:cursor-pointer hover:bg-primary-200 p-1.5 rounded-full transition-all">
             <CirclePlus />
           </button>
@@ -34,7 +35,7 @@ export default function ContactLeft() {
           </button>
         </div>
       </div>
-      <div id="contact-search" className="my-2">
+      <div id="contact-search" className="p-2">
         <input
           type="text"
           name="contact-search"
@@ -43,10 +44,31 @@ export default function ContactLeft() {
           placeholder="Search or start a new conversation"
         />
       </div>
-      <ul id="contacts" className="flex flex-col gap-y-2">
+      <ul id="contacts" className="flex flex-col gap-y-2 px-2 overflow-auto">
         {filteredContacts &&
           filteredContacts.map((contact) => (
-            <li key={contact.id}>{contact.first_name + contact.last_name}</li>
+            <li key={contact.id}>
+              <div className="flex items-center gap-3 p-2 rounded-xl hover:bg-muted transition-colors cursor-pointer hover:bg-primary-200">
+                <div className="relative w-12 h-12 shrink-0 overflow-hidden rounded-full shadow-sm">
+                  <Image
+                    src={`${contact.profile_pic}`}  
+                    alt={`${contact.first_name} ${contact.last_name}`}
+                    fill
+                    className="object-cover"
+                    sizes="48px"
+                    priority
+                  />
+                </div>
+                <div className="flex flex-col">
+                <p className="font-medium truncate">
+                  {contact.first_name} {contact.last_name}
+                </p>
+                <p className="text-[12px] text-text-600">
+                  {contact.message.slice(0,30) + "..."}
+                </p>
+                </div>
+              </div>
+            </li>
           ))}
       </ul>
     </div>
